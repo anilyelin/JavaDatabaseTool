cbbc9e8fe1616d0c635e4358e3c4e449c682c7dc
anilyelin <dsadsad>
2020-01-13 18:33:29 +0100

    bug fixing

--- hellofx/src/main/java/org/openjfx/hellofx/AccountController.java
+++ hellofx/src/main/java/org/openjfx/hellofx/AccountController.java
@@ -161,7 +161,7 @@ public class AccountController {
 		}
 	}
 	
-	//here to change
+	
 	public String hashPassword(String base) {
 		try {
 			MessageDigest digest = MessageDigest.getInstance("SHA-256");
